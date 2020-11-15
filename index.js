7c302eb90b15893eb732964b80d0fed3ee1b6998
abolfazl <abolfazljdst@gmail.com>
2020-11-15 15:29:05 +0330

    debugging

--- index.js
+++ index.js
@@ -15,56 +15,72 @@ let menha = document.getElementById('menha')
 let assign = document.getElementById('assign')
 let fac = document.getElementById('fac')
 let y = ''
+let clear = true
 let x
 for (let i = 0; i < boxes.length; i++) {
     const box = boxes[i];
     box.onclick = function() {
         x = box.textContent
-        y = y.concat(x)
+        if (clear) y = y.concat(x)
+        else {
+            y = x.toLocaleString()
+            clear = true
+        }
         textarea.textContent = y
     }
 }
 sin.onclick = () => {
     textarea.textContent = Math.sin(parseInt(y))
     y = textarea.textContent
+    clear = false
 }
 cos.onclick = () => {
     textarea.textContent = Math.cos(parseInt(y))
     y = textarea.textContent
+    clear = false
 }
 tan.onclick = () => {
     textarea.textContent = Math.tan(parseInt(y))
     y = textarea.textContent
+    clear = false
 }
 sqrt.onclick = () => {
     textarea.textContent = Math.sqrt(parseInt(y))
     y = textarea.textContent
+    clear = false
 }
 logg.onclick = () => {
     textarea.textContent = Math.log(parseInt(y))
     y = textarea.textContent
+    clear = false
 }
 tavan2.onclick = () => {
     textarea.textContent = Math.pow(parseInt(y), 2)
     y = textarea.textContent
+    clear = false
 }
 tavan3.onclick = () => {
     textarea.textContent = Math.pow(parseInt(y), 3)
     y = textarea.textContent
+    clear = false
 }
 sum.onclick = () => {
+    clear = true
     y = y.concat('+')
     textarea.textContent = y
 }
 zarb.onclick = () => {
+    clear = true
     y = y.concat('*')
     textarea.textContent = y
 }
 bar.onclick = () => {
+    clear = true
     y = y.concat('/')
     textarea.textContent = y
 }
 menha.onclick = () => {
+    clear = true
     y = y.concat('-')
     textarea.textContent = y
 }
@@ -73,12 +89,14 @@ fac.onclick = () => {
     y = parseInt(y)
     textarea.textContent = factorial(y)
     y = textarea.textContent
+    clear = false
 }
 
 assign.onclick = () => {
     textarea.textContent = eval(y)
     let z = eval(y)
     y = z.toLocaleString()
+    clear = false
 }
 
 reset.onclick = function() {
